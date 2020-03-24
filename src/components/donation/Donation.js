62233ff4e82a75e14cb856cf05dd004d85128c54
Roman <gnatyuk.info@gmail.com>
2020-03-24 13:05:34 +0200

    almost

--- src/components/donation/Donation.js
+++ src/components/donation/Donation.js
@@ -3,9 +3,12 @@ import Typography from '@material-ui/core/Typography';
 
 export const Donation = () => {
   return (
-    <Typography>
-      Не кожен має змогу потрапити в магазин і запастися туалетними рулонами. Не
-      будьте егоїстичними.
-    </Typography>
+    <>
+      <Typography>
+        Не кожен має змогу потрапити в магазин і запастися туалетними рулонами.
+        Не будьте егоїстичними.
+      </Typography>
+      <Typography></Typography>
+    </>
   );
 };
