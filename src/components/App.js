7dae2388e029cc3738d00366f1aeec7566681fe5
Roman <gnatyuk.info@gmail.com>
2020-03-24 13:09:52 +0200

    del smth

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { store } from '../redux/store';
-import { Provider } from 'react-redux';
 import { Sliders } from './sliders/Sliders';
 
 export const App = () => {
-  return (
-    <Provider store={store}>
-      <Sliders />
-    </Provider>
-  );
+  return <Sliders />;
 };
